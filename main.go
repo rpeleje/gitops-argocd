a5fae185d49675907b42b2806ac12b6c2caa819c
rmoreirap <rmoreirap@indracompany.com>
2022-05-25 18:36:46 -0300

    teste argocd

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("<h1>Hello Peleje!!!</h1>"))
+		w.Write([]byte("<h1>Hello Peleje!!!</h1> <h2>SHOW!</h2>"))
 	})
 	http.ListenAndServe(":4001", nil)
 }
